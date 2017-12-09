fd80425c4406f418454d3f5761d9afab16aa03df
JBeto <jbeto001@ucr.edu>
2017-12-09 00:40:50 -0800

    - Infinite loop error

--- exec.c
+++ exec.c
@@ -63,11 +63,11 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-  int test;
-  if((test = allocuvm(pgdir, KERNBASE - PGSIZE, KERNBASE - PGSIZE + 1)) == 0)
+  uint stack = KERNBASE - (2 * PGSIZE);
+  if(allocuvm(pgdir, stack, KERNBASE) == 0)
     goto bad;
+  clearpteu(pgdir, (char*)stack);
   sp = USERSTACK;
-  cprintf("alloc: %x", test);
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
@@ -98,6 +98,7 @@ exec(char *path, char **argv)
   oldpgdir = curproc->pgdir;
   curproc->pgdir = pgdir;
   curproc->sz = sz;
+  curproc->pgcount = 1;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
   switchuvm(curproc);
